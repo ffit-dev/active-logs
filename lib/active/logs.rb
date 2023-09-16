3a2e6031b4021f4386eea3d02c6ce6e68421880d
unimafy <nima.yonten1729@gmail.com>
2023-09-16 21:37:03 +0600

    initial commit

---
+++ lib/active/logs.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "logs/version"
+
+module Active
+  module Logs
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/active/logs/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Active
+  module Logs
+    VERSION = "0.1.0"
+  end
+end
